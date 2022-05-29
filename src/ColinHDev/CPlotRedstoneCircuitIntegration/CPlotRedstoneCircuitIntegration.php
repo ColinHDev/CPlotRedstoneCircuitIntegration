1371099a78510a2abf4891554c6cdb122a023230
ColinHDev <colinheidfeld@gmail.com>
2022-05-29 16:38:42 +0200

    added main class

---
+++ src/ColinHDev/CPlotRedstoneCircuitIntegration/CPlotRedstoneCircuitIntegration.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ColinHDev\CPlotRedstoneCircuitIntegration;
+
+use pocketmine\plugin\PluginBase;
+
+class CPlotRedstoneCircuitIntegration extends PluginBase {
+
+    public function onEnable() : void {
+    }
+}
